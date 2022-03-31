38ece5c250a16d997e96c3b1614659e852fba0ed
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-03-31 16:03:42 -0400

    added to server.js and main.handlebars

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = require('express');
 const path = require('path');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 // turn on routes
 app.use(routes);
 
